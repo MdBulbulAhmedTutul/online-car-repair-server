6592a9ce0595da547da978eddcd3c54b26c2b6cd
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-21 03:02:22 -0800

    added the mongodb user and pass

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // midleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sq1fqp2.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // OUR DB
+        
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('online car repair running')
