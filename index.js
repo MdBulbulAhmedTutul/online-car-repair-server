5d389c44a203acca6a4d7f2295e060d5f1d3aad7
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-21 02:54:25 -0800

    server side file ready

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('online car repair running')
+})
+
+app.listen(port, () => {
+    console.log(`online car repair running on port ${port}`)
+})
